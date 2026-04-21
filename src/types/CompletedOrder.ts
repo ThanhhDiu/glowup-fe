fce836accc9c53ce9877497c0d8b5cead02ee5fe
ThanhhDiu <vonguyenthanhdieu111@gmail.com>
2026-04-21 20:31:38 +0700

    feat: create component for complete tab

---
+++ src/types/CompletedOrder.ts
@@ -0,0 +1,10 @@
+export interface CompletedOrder {
+    id: string;
+    serviceName: string;
+    subService: string;
+    customerName: string;
+    technicianName: string;
+    completionDate: string;
+    totalPrice: number;
+    rating: number; // Số sao đánh giá (0 nếu chưa đánh giá)
+}
